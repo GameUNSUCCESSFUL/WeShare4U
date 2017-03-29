5f6ea2a9d53b59b2e67481124c9b2ba55cb8cbf2
สุขพัฒน์ เทพารส <cakem2050@gmail.com>
2017-03-29 23:13:19 +0700

    เทส

---
+++ application/views/home.php
@@ -0,0 +1,45 @@
+<html>
+<style>
+    .in {
+        margin: 5px;
+        padding: 2px 10px 2px;
+        border-radius: 5px;
+        border: 1px solid #8c8c8c;
+        width:200px;
+        font-family: "TH SarabunPSK", Times, serif;
+        font-size: 25px;
+    }
+    .in:focus {
+        size: 100px;
+        outline: none;
+        box-shadow: 0 0 3px #3782ca;
+        font-family: "TH SarabunPSK", Times, serif;
+        font-size: 25px;
+    }
+    form{
+        font-family: "TH SarabunPSK", Times, serif;
+        font-size: 25px;
+    }
+    input[type=submit]{
+        margin: 5px;
+        padding: 10px;
+        border-radius: 5px;
+        color: #f6f2f8;
+        background-color: #4F5155;
+        border: 1px solid #8c8c8c;
+    }
+
+
+
+</style>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <form>
+        Username : <input class="in" type="text"><br>
+        Password : <input class="in" type="password">
+        <input type="submit" value="เข้าสู่ระบบ">
+    </form>
+    </body>
+</html>
