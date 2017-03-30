0034ae8641312dbd5791d8c1ff1b3677c14849e8
JuJiiz <ju.jiiz.1579@gmail.com>
2017-03-31 00:25:00 +0700

    MVC receiver

--- application/models/ReceiverModel.php
+++ application/models/ReceiverModel.php
@@ -12,4 +12,5 @@ class ReceiverModel extends CI_Model
     {
         parent::__construct();
     }
+
 }
--- application/views/receiverView.php
+++ application/views/receiverView.php
@@ -1,27 +1,33 @@
 <!DOCTYPE HTML>
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <title>Item Grid</title>
+
 </head>
 <body>
 <h1 align="center">Show Data</h1>
 
-<div class="row">
-    <?php foreach ($rs as $r):?>
-    <div class="col-md-3">
-        <div class="block">
-            <div class="thumbnail">
-                <!--<img src="img/img1.jpg" alt="" class="img-responsive">-->
-                <div class="caption">
-                    <h1> <?php echo $r['product_name'] ?> </h1>
-                    <a class="btn" href="#">more</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php endforeach; ?>
-</div>
+<div class="container">
+    <div class="featured-block">
 
+        <div class="row">
+            <?php foreach ($dbcon as $r): ?>
+                <div class="col-md-3">
+                    <div class="block">
+                        <div class="thumbnail">
+                            <!--<img src="img/img1.jpg" alt="" class="img-responsive">-->
+                            <div class="caption">
+                                <h1> <?php echo $r['product_name'] ?> </h1>
+                                <a class="btn" href="#">see more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+    </div>
+</div>
 
 <?php
 //    if(count($rs)==0){
