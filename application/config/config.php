a47c70514a21642cef32d34a95a1fe9ae6c256dd
gamelolo <saytanpotong@gmail.com>
2017-03-30 22:45:49 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/WeShare4U/';
+$config['base_url'] = 'http://localhost/WeShared4U/';
 
 /*
 |--------------------------------------------------------------------------
