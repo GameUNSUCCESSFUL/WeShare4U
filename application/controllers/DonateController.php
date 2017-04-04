c0a4c13df621a5af6c4e5cade30308096608a091
GAME-PC\Game\repo <saytanpotong@gmail.com>
2017-04-05 01:30:01 +0700

    donor form

--- application/controllers/DonateController.php
+++ application/controllers/DonateController.php
@@ -78,6 +78,7 @@ class DonateController extends CI_Controller
      */
     public function do_edit()
     {
+
         $img_name = time() . rand();
         $config = array(
             'upload_path' => 'uploads/donateImages',
@@ -89,36 +90,51 @@ class DonateController extends CI_Controller
         );
         $this->load->library('upload', $config);
 
-        $product_id = $this->input->post("product_id");
-        $product_name = $this->input->post("product_name");
-        $product_color = $this->input->post("product_color");
-        $product_number = $this->input->post("product_number");
+        $this->form_validation->set_rules('product_name', 'product_name', 'required');
+        $this->form_validation->set_rules('product_color', 'product_color', 'required');
+        $this->form_validation->set_rules('product_number', 'product_number', 'required');
+        $this->form_validation->set_rules('weight_number', 'weight_number', 'required');
+        $this->form_validation->set_rules('weight_type', 'weight_type', 'required');
+        $this->form_validation->set_rules('size_width', 'size_width', 'required');
+        $this->form_validation->set_rules('size_long', 'size_long', 'required');
+        $this->form_validation->set_rules('size_type', 'size_type', 'required');
+        $this->form_validation->set_rules('product_detail', 'product_detail', 'required');
+        $this->form_validation->set_rules('product_type', 'product_type', 'required');
 
-        $weight_number = (string)$this->input->post("weight_number");
-        $weight_type = $this->input->post("weight_type");
+        if($this->form_validation->run() === false){
+            echo "error";
+        }else{
+            $product_id = $this->input->post("product_id");
+            $product_name = $this->input->post("product_name");
+            $product_color = $this->input->post("product_color");
+            $product_number = $this->input->post("product_number");
 
-        $size_width = (string)$this->input->post("size_width");
-        $size_long = (string)$this->input->post("size_long");
-        $size_type = $this->input->post("size_type");
+            $weight_number = (string)$this->input->post("weight_number");
+            $weight_type = $this->input->post("weight_type");
 
-        $product_detail = $this->input->post("product_detail");
-        $product_type = $this->input->post("product_type");
-        $user_id = 1;
+            $size_width = (string)$this->input->post("size_width");
+            $size_long = (string)$this->input->post("size_long");
+            $size_type = $this->input->post("size_type");
 
-        if ($_FILES['product_image']['size'] == 0)
-        {
+            $product_detail = $this->input->post("product_detail");
+            $product_type = $this->input->post("product_type");
+            $user_id = 1;
+
+            if ($_FILES['product_image']['size'] == 0)
+            {
                 $img_path = $_SESSION['img_path'];
 
                 $this->DonorModel->edit_product($product_id, $product_name, $product_color, $product_number, $weight_number, $weight_type, $size_width, $size_long, $size_type, $product_detail,  $product_type, $user_id, $img_path);
 
                 $this->show_last_donate($product_id);
-        }else{
-            if ($this->upload->do_upload('product_image')) {
-                $img_path = $img_name . $this->upload->data('file_ext');
-                $this->DonorModel->edit_product($product_id, $product_name, $product_color, $product_number, $weight_number, $weight_type, $size_width, $size_long, $size_type, $product_detail,  $product_type, $user_id, $img_path);
-                $this->show_last_donate($product_id);
-            } else {
-                echo $this->upload->display_errors();
+            }else{
+                if ($this->upload->do_upload('product_image')) {
+                    $img_path = $img_name . $this->upload->data('file_ext');
+                    $this->DonorModel->edit_product($product_id, $product_name, $product_color, $product_number, $weight_number, $weight_type, $size_width, $size_long, $size_type, $product_detail,  $product_type, $user_id, $img_path);
+                    $this->show_last_donate($product_id);
+                } else {
+                    echo $this->upload->display_errors();
+                }
             }
         }
 
