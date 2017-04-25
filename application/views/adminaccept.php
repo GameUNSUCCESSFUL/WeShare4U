5a1a414cb76e3ead51ee447be793aed8d2ae306f
สุขพัฒน์ <>
2017-04-26 04:09:32 +0700

    no message

--- application/views/adminaccept.php
+++ application/views/adminaccept.php
@@ -14,16 +14,14 @@
 <!-- bar -->
 
 <?php include "navbaradmin.php" ?>
-
+<div style="height: 100vh">
 <div class="container">
     <br>
     <div class="thumbnail">
         <br><br>
-        <div align="center"><h1 >ยินดีต้อนรับ</h1></div>
+        <div align="center"><h1 style="padding-top: 150px;padding-bottom: 150px">ยินดีต้อนรับ</h1></div>
         <br><br><br>
     </div>
-</div>
-</div>
 </div>
 </div>
 <!-- footer -->
